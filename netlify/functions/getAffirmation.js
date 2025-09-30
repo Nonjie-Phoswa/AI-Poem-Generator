8ad8a746a12642082e23ed0757736a57fe7475b9
nophoswjhb024 <nonjie.phoswa@gmail.com>
2025-09-30 13:31:23 +0200

    updated api prompt

--- netlify/functions/getAffirmation.js
+++ netlify/functions/getAffirmation.js
@@ -12,7 +12,7 @@ exports.handler = async function (event) {
 
   let prompt = `Create daily affirmations about ${topic} and in the language of the ${topic}. The affirmations must start with a title written in ALL CAPS on the first line. Do not use asterisks or special characters around the title. Each affirmation must be positive, unisex, written in the first person and if it's inpired by the Bible, include a short scripture reference in brackets at the end. Do not number or format them with symbols. Just return plain text.`;
 
-  let context = "Make it creative, engaging and less than 120 words.";
+  let context = "Make it creative, engaging, written in a poetic structure and less than 120 words.";
 
   let url = `https://api.shecodes.io/ai/v1/generate?prompt=${encodeURIComponent(
     prompt
